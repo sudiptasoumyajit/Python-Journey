f79b6ba7a4bca9fcc3ba6043d7caade70865cf56
sudiptasoumyajit <84839625+sudiptasoumyajit@users.noreply.github.com>
2022-04-11 16:05:43 +0530

    Add files via upload

--- c155-hw/temp.py
+++ c155-hw/temp.py
@@ -1,1 +1,15 @@
-Copyright, Sudipta Soumyajit
+from tkinter import *
+root = Tk()
+root.title("What MC Textures Says About You")
+root.geometry("200x400")
+x = []
+data = {
+            'Default Texture':'You Are New To The Game Or You Do Not Know How To Install It',
+            'Pink Texture':'You Are A Girl Who Likes Pink Setup',
+            'Anime Texture':'You Like Animes Right?',
+            'Colored Texture':'You Like A Particular Color Like: Orange, Green, Yellow .etc',
+            'Ultra Realastic Texture':'You Want Minecraft To Be In Reality, Right?',
+            'YT Texture':'You Like Any YouTuber Right?'
+        }
+print(data)
+root.mainloop()
